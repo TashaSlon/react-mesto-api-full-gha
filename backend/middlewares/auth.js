9c04b61bf0b6ee9908bcda3a46c57b8929b4bdc9
plyusnina <plyusnina@live.ru>
2023-07-11 13:04:04 +0500

    auth fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,30 +5,15 @@ const NotAuthError = require('../errors/not-auth-err');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const YOUR_JWT = req.cookies.jwt;
-  const SECRET = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
+  const token = req.cookies.jwt;
+  let payload;
+
   try {
-    const payload = jwt.verify(YOUR_JWT, SECRET);
-    req.user = payload;
-    console.log('\x1b[31m%s\x1b[0m', `
-    Надо исправить. В продакшне используется тот же
-    секретный ключ, что и в режиме разработки.
-    `);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
-      console.log(
-        '\x1b[32m%s\x1b[0m',
-        'Всё в порядке. Секретные ключи отличаются',
-      );
-    } else {
-      console.log(
-        '\x1b[33m%s\x1b[0m',
-        'Что-то не так',
-        err,
-      );
-    }
     next(new NotAuthError('Необходима авторизация'));
   }
 
+  req.user = payload;
   next();
 };
